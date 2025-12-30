5497a40a0ef7937f2e2be2dca35b2fe5f77d3d7d
GodofDrakes <GodofDrakes@pm.me>
2025-12-30 12:48:44 -0800

    Initial project

---
+++ GodofDrakes.CommandPalette.Reactive/ListItemView.cs
@@ -0,0 +1,40 @@
+﻿using System.Reactive.Disposables;
+using Microsoft.CommandPalette.Extensions.Toolkit;
+
+namespace GodofDrakes.CommandPalette.Reactive;
+
+public partial class ListItemView : ListItem, IDisposable
+{
+	private readonly CompositeDisposable _onDispose = [];
+
+	public static ListItemView Create( Func<ListItemView, IDisposable?> configure )
+	{
+		ArgumentNullException.ThrowIfNull( configure );
+
+		var view = new ListItemView();
+
+		var disposable = configure( view );
+
+		if ( disposable is not null )
+		{
+			view._onDispose.Add( disposable );
+		}
+
+		return view;
+	}
+
+	protected virtual void Dispose( bool disposing )
+	{
+		if ( disposing )
+		{
+			_onDispose.Dispose();
+		}
+	}
+
+	public void Dispose()
+	{
+		Dispose( true );
+
+		GC.SuppressFinalize( this );
+	}
+}
